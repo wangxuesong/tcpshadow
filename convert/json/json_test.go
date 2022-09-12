a5a320ecee3011742c5078103c1937a6f11b19bc
Martin Wang <wangxuesong@gmail.com>
2022-09-12 12:45:51 +0800

    add tests for convert json

--- convert/json/json_test.go
+++ convert/json/json_test.go
@@ -45,3 +45,98 @@ func TestConvert_SqliCommand_Json(t *testing.T) {
 		assert.Equal(t, test.json, buf, fmt.Sprintf("Test Failed: %s", test.name))
 	}
 }
+
+func TestConvert_SavePackage_Json(t *testing.T) {
+	tests := []struct {
+		name       string
+		json       string
+		count      int
+		assertFunc func(t *testing.T, packages []model.SavePackage, name string)
+	}{{
+		name:  "empty",
+		json:  `{"name":"test"}`,
+		count: 2,
+		assertFunc: func(t *testing.T, packages []model.SavePackage, _ string) {
+			assert.Equal(t, 0, packages[0].Length)
+			assert.Equal(t, 0, packages[1].Length)
+		},
+	}, {
+		name: "auth",
+		json: `{
+  "name": "test",
+  "auth": [
+    {
+      "number": 0,
+      "forward": 0,
+      "length": 489,
+      "buffer": "AekBPAAAAGQAZQAAAD0ABklFRUVNAABsc3FsZXhlYwAAAAAAAAY5LjI4MAAADFJEUyNSMDAwMDAwAAAFc3FsaQAAAAE8AAAAAAAAAAAAAQAJZ2Jhc2VkYnQAACEBAQEBAQEBAWx2WnB4YmdNRnd4OGpycGVlaWNRRVE9PQBvbAAAAAAAAAAAAD10bGl0Y3AAAAAAAAEAaAALAAAAAwAOb2xfZ2Jhc2VkYnRfMQAAAAAAAAAAAAAAAGoABgAHREJQQVRIAAACLgAAB0RCREFURQAABlk0TUQtAAAMSUZYX1VQRERFU0MAAAIxAAARQ0xOVF9QQU1fQ0FQQUJMRQAAAjEAAAhTUUxNT0RFAAAGZ2Jhc2UAAAlOT0RFRkRBQwAAA25vAABrAAAAAAAAAAAAAAABABFNYXJ0aW5Qcm8tODMubGFuAAAAACsvVXNlcnMvbWFydGluL3Byb2plY3RzLzhzcHJvamVjdHMvSkRCQ1Rlc3QAAHQAbwAAAAAAAAAAAGUvVXNlcnMvbWFydGluL3Byb2plY3RzLzhzcHJvamVjdHMvSkRCQ1Rlc3QvbGliL2diYXNlZGJ0amRiY18zLjMuMF8yLmphckNvbm5lY3Rpb25UZXN0L0Nvbm5lY3Rpb25UZXN0AAB/"
+    },
+    {
+      "number": 1,
+      "forward": 1,
+      "length": 267,
+      "buffer": "AQsCPBAAAGQAZQAAAD0ABklFRUVJAABsc3J2aW5meAAAAAAAACNHQmFzZSBTZXJ2ZXIgVmVyc2lvbiA5LjU2LkZDNEcxQUVFAAAjU29mdHdhcmUgU2VyaWFsIE51bWJlciBBQUEjQjAwMDAwMAAADm9sX2diYXNlZGJ0XzEAAAABPAAAAAAAAAAAAAAAAAAAb24AAAAAAAAAAAA9c29jdGNwAAAAAAAAAGYAAAAAAAAAAAAAABQAAABrAAAAAAAAAAwAAAAAAA1kOTAzOGRhNWE4MjQAAAAAAi8AAG4ABAAAAAAAdAAiAAAD6AAAAAAAGC9vcHQvZ2Jhc2U4cy9iaW4vb25pbml0AAB/"
+    }
+  ]
+}`,
+		count: 2,
+		assertFunc: func(t *testing.T, packages []model.SavePackage, _ string) {
+			assert.Equal(t, model.ClientToServer, packages[0].Forward)
+			assert.Equal(t, 489, packages[0].Length)
+			assert.Equal(t, model.ServerToClient, packages[1].Forward)
+			assert.Equal(t, 267, packages[1].Length)
+		},
+	}, {
+		name: "protocol",
+		json: `{
+  "name": "test",
+  "auth": [
+    {
+      "number": 0,
+      "forward": 0,
+      "length": 489,
+      "buffer": "AekBPAAAAGQAZQAAAD0ABklFRUVNAABsc3FsZXhlYwAAAAAAAAY5LjI4MAAADFJEUyNSMDAwMDAwAAAFc3FsaQAAAAE8AAAAAAAAAAAAAQAJZ2Jhc2VkYnQAACEBAQEBAQEBAWx2WnB4YmdNRnd4OGpycGVlaWNRRVE9PQBvbAAAAAAAAAAAAD10bGl0Y3AAAAAAAAEAaAALAAAAAwAOb2xfZ2Jhc2VkYnRfMQAAAAAAAAAAAAAAAGoABgAHREJQQVRIAAACLgAAB0RCREFURQAABlk0TUQtAAAMSUZYX1VQRERFU0MAAAIxAAARQ0xOVF9QQU1fQ0FQQUJMRQAAAjEAAAhTUUxNT0RFAAAGZ2Jhc2UAAAlOT0RFRkRBQwAAA25vAABrAAAAAAAAAAAAAAABABFNYXJ0aW5Qcm8tODMubGFuAAAAACsvVXNlcnMvbWFydGluL3Byb2plY3RzLzhzcHJvamVjdHMvSkRCQ1Rlc3QAAHQAbwAAAAAAAAAAAGUvVXNlcnMvbWFydGluL3Byb2plY3RzLzhzcHJvamVjdHMvSkRCQ1Rlc3QvbGliL2diYXNlZGJ0amRiY18zLjMuMF8yLmphckNvbm5lY3Rpb25UZXN0L0Nvbm5lY3Rpb25UZXN0AAB/"
+    },
+    {
+      "number": 1,
+      "forward": 1,
+      "length": 267,
+      "buffer": "AQsCPBAAAGQAZQAAAD0ABklFRUVJAABsc3J2aW5meAAAAAAAACNHQmFzZSBTZXJ2ZXIgVmVyc2lvbiA5LjU2LkZDNEcxQUVFAAAjU29mdHdhcmUgU2VyaWFsIE51bWJlciBBQUEjQjAwMDAwMAAADm9sX2diYXNlZGJ0XzEAAAABPAAAAAAAAAAAAAAAAAAAb24AAAAAAAAAAAA9c29jdGNwAAAAAAAAAGYAAAAAAAAAAAAAABQAAABrAAAAAAAAAAwAAAAAAA1kOTAzOGRhNWE4MjQAAAAAAi8AAG4ABAAAAAAAdAAiAAAD6AAAAAAAGC9vcHQvZ2Jhc2U4cy9iaW4vb25pbml0AAB/"
+    }
+  ],
+  "package": [
+    {
+      "number": 2,
+      "forward": 0,
+      "command": [
+        {
+          "type": "SQ_PROTOCOLS",
+          "sqli": {
+            "Protocol": "//x//DyMqpcQ"
+          }
+        },
+        {
+          "type": "SQ_EOT",
+          "sqli": {}
+        }
+      ]
+    }
+  ]
+}`,
+		count: 3,
+		assertFunc: func(t *testing.T, packages []model.SavePackage, _ string) {
+			assert.Equal(t, model.ClientToServer, packages[2].Forward)
+			assert.Equal(t, 16, packages[2].Length)
+			bytes := []byte{0, 126, 0, 9, 255, 252, 127, 252, 60, 140, 170, 151, 16, 0, 0, 12}
+			assert.Equal(t, bytes, packages[2].Buffer)
+		},
+	}}
+
+	for _, test := range tests {
+		pkg, err := UnmarshalSqliSavePackages(test.json)
+		require.NoError(t, err)
+		assert.IsType(t, []model.SavePackage{}, pkg)
+		assert.Equal(t, test.count, len(pkg))
+		test.assertFunc(t, pkg, test.name)
+	}
+}
