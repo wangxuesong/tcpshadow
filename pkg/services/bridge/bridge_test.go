4b73e7d5dd65c689d21e518883a87cfe61f36ace
zhangyaru <2297195082@qq.com>
2023-01-06 09:44:07 +0800

    增加测试“TestConnectStart”，测试StartMessage状态

--- pkg/services/bridge/bridge_test.go
+++ pkg/services/bridge/bridge_test.go
@@ -1303,8 +1303,8 @@ func TestQueryFilter_Handle_SELECT(t *testing.T) {
 	//assert.True(t, server_passed)
 }
 
-func TestConnect(t *testing.T) {
-	pgFront, pgBackend := net.Pipe()
+func TestConnectStart(t *testing.T) {
+	_, pgBackend := net.Pipe()
 	gbFront, gbBackend := net.Pipe()
 	filter := NewConnectFilter()
 	ctx := &Context{
@@ -1314,6 +1314,7 @@ func TestConnect(t *testing.T) {
 		state:     ConnectState,
 		metadata:  make(map[string]interface{}),
 	}
+
 	msg := &pgproto.StartupMessage{
 		ProtocolVersion: 196608,
 		Parameters: map[string]string{
@@ -1334,166 +1335,23 @@ func TestConnect(t *testing.T) {
 	// 8s Server
 	go func() {
 		buf := make([]byte, 1024)
-		// read AuthRequest
 		c, err := gbBackend.Read(buf)
-		assert.Nil(t, err)
-		assert.True(t, c > 0)
-		//read SqliProtocols
-		c, err = gbBackend.Read(buf)
 		buff := buf[:c]
 		re := bytes.NewReader(buff)
-		msgs, err := model.UnpackSqliTransmission(re)
+		request := &model.AuthRequest{}
+		err = request.Unpack(re)
 		assert.Nil(t, err)
-		assert.IsType(t, &model.SqliProtocols{}, msgs[0])
-		assert.IsType(t, &model.SqliEot{}, msgs[1])
-		assert.Nil(t, err)
-		assert.True(t, c > 0)
-		//read SqliInfo
-		c, err = gbBackend.Read(buf)
-		buff = buf[:c]
-		re = bytes.NewReader(buff)
-		msgs, err = model.UnpackSqliTransmission(re)
-		assert.Nil(t, err)
-		assert.IsType(t, &model.SqliInfo{}, msgs[0])
-		assert.IsType(t, &model.SqliEot{}, msgs[1])
-		assert.Nil(t, err)
-		assert.True(t, c > 0)
-		//read SqliDBOpen
-		c, err = gbBackend.Read(buf)
-		assert.Nil(t, err)
-		assert.True(t, c > 0)
-		buff = buf[:c]
-		re = bytes.NewReader(buff)
-		msgs, err = model.UnpackSqliTransmission(re)
-		assert.Nil(t, err)
-		assert.IsType(t, &model.SqliDBOpen{}, msgs[0])
-		assert.IsType(t, &model.SqliEot{}, msgs[1])
-	}()
-
-	go func() {
-		err := filter.Handle(ctx)
-		assert.Nil(t, err)
-		// AuthResponse
-		buff, err := (&model.AuthResponse{
-			Length:           287,
-			Noname1:          2,
-			Noname2:          15376,
-			Noname3:          0,
-			Noname4:          100,
-			Noname5:          101,
-			Noname6:          61,
-			IEEEIlength:      6,
-			IEEEI:            "IEEEI",
-			Noname7:          108,
-			Srvinfx:          "srvinfx",
-			Versionlength:    34,
-			Version:          "GBase Server Version 9.56.FC4G1TL",
-			Softwarelength:   35,
-			Software:         "Software Serial Number AAA#B000000",
-			Clientnamelength: 12,
-			Clientname:       "gbaseserver",
-			Noname8:          316,
-			Noname9:          0,
-			Noname10:         0,
-			Noname11:         0,
-			Noname12:         0,
-			Noname13:         0,
-			Noname14:         "on",
-			Noname15:         "=soctcp",
-			Noname16:         102,
-			Noname17:         0,
-			Noname18:         0,
-			Noname19:         20,
-			Noname20:         0,
-			Noname21:         107,
-			Noname22:         2958,
-			Noname23:         872,
-			Noname24:         13312,
-			Path1length:      11,
-			Path1:            "/dev/pts/0",
-			Path2length:      15,
-			Path2:            "/home/gbasedbt",
-			Noname25:         110,
-			Noname26:         4,
-			Noname27:         0,
-			Noname28:         0,
-			Noname29:         116,
-			Noname30:         43,
-			Noname31:         0,
-			Noname32:         1001,
-			Noname33:         0,
-			Noname34:         1001,
-			Path3length:      33,
-			Path3:            "/home/zhangyaru/gbase/bin/oninit",
-			Asceot:           127,
-		}).Pack()
-		assert.Nil(t, err)
-		ctx.SetData(&model.Data{
-			Forward: model.ServerToClient,
-			Buffer:  buff,
-		})
-		err = filter.Handle(ctx)
-		assert.Nil(t, err)
-		// SqliProtocols
-		protocol := []byte{0, 126, 0, 9, 189, 190, 159, 254, 127, 183, 255, 239, 240, 0}
-		eot, err := (&model.SqliEot{}).Pack()
-		assert.Nil(t, err)
-		for _, c := range eot {
-			protocol = append(protocol, c)
-		}
-		ctx.SetData(&model.Data{
-			Forward: model.ServerToClient,
-			Buffer:  protocol,
-		})
-		err = filter.Handle(ctx)
-		assert.Nil(t, err)
-		// SqliInfo
-		ctx.SetData(&model.Data{
-			Forward: model.ServerToClient,
-			Buffer:  eot,
-		})
-		err = filter.Handle(ctx)
-		assert.Nil(t, err)
-		// SqliDBOpen
-		done := &model.SqliDone{
-			Warning:  21,
-			Rows:     0,
-			RowID:    0,
-			SerialID: 0,
-		}
-		cost := &model.SqliCost{
-			EstimatedRows: 1,
-			EstimatedIO:   1,
-		}
-		eott := &model.SqliEot{}
-		var transmission model.SqliTransmission
-		transmission = []model.SqliCommand{done, cost, eott}
-		buf, err = transmission.Pack()
-		assert.Nil(t, err)
-		ctx.SetData(&model.Data{
-			Forward: model.ServerToClient,
-			Buffer:  buf,
-		})
-		err = filter.Handle(ctx)
+		assert.IsType(t, &model.AuthRequest{}, request)
 		assert.Nil(t, err)
 	}()
 
-	{
-		parse, err := pgproto.NewFrontend(pgproto.NewChunkReader(pgFront), nil)
-		assert.Nil(t, err)
-		msg, err := parse.Receive()
-		assert.Nil(t, err)
-		assert.IsType(t, &pgproto.Authentication{}, msg)
-		msg, err = parse.Receive()
-		assert.Nil(t, err)
-		assert.IsType(t, &pgproto.ParameterStatus{}, msg)
-		msg, err = parse.Receive()
-		assert.Nil(t, err)
-		assert.IsType(t, &pgproto.BackendKeyData{}, msg)
-		msg, err = parse.Receive()
-		assert.Nil(t, err)
-		assert.IsType(t, &pgproto.ReadyForQuery{}, msg)
-	}
+	err := filter.Handle(ctx)
+	assert.Nil(t, err)
+	v, err := ctx.MetaData("ConnectStage")
+	assert.Nil(t, err)
+	stage, ok := v.(string)
+	assert.Equal(t, ok, true)
+	assert.Equal(t, stage, "ConnectProtocol")
 }
 
 func TestConnectPro(t *testing.T) {
